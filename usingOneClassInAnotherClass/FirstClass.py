bffb0be8cbb59f7ab6645a059e23b9186d8d7020
Ankit <ssingh3.ankit@gmail.com>
2019-09-17 23:47:47 +0530

    first commit

--- usingOneClassInAnotherClass/FirstClass.py
+++ usingOneClassInAnotherClass/FirstClass.py
@@ -5,3 +5,8 @@ class FirstClass:
 
     def method1(self):
         print("value of variable is: " + self.var1)
+
+if __name__=="__main__":
+    print("this class has been run directly")
+else:
+    print("doesnt")
--- usingOneClassInAnotherClass/SecondFile.py
+++ usingOneClassInAnotherClass/SecondFile.py
@@ -5,6 +5,14 @@ class SecondFile():
     def __init__(self):
         print("Inside second class")
 
+    def method2(self):
+        print("Inside second class method2")
+
 firstClass = FirstClass("firstclass")
 firstClass.method1()
+
+secondClass = SecondFile()
+
+if __name__=="__main__":
+    secondClass.method2()
 
